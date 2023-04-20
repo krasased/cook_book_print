717e83a99c4f6d97ae6fd8b82a08480193a937d5
Alexey Sedykh <alexey.e.sedykh@yandex.ru>
2023-04-20 17:48:58 +0300

    commit with calculation

--- main.py
+++ main.py
@@ -16,4 +16,30 @@ with open('recipes.txt', 'rt', encoding='utf-8') as file:
         file.readline()
         cook_book[dish_name] = ingrids
 
-    pprint(cook_book, indent=2, sort_dicts=False, width=144, compact=False)
+pprint(cook_book, indent=2, sort_dicts=False, width=144, compact=False)
+
+def get_shop_list_by_dishes(dishes, pers_num):
+    res = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for ingrid in cook_book[dish]:
+                if ingrid['ingrid_name'] in res:
+                    res[ingrid['ingrid_name']]['qty'] += int(ingrid['qty']) * pers_num
+                else:
+                    res[ingrid['ingrid_name']] = {'measure': ingrid['measure'],'qty': (int(ingrid['qty']) * pers_num)}
+        else:
+            print('\nНекоторых блюд нет в книге рецептов')
+    print(f"\nКоличество гостей: {pers_num}\n Необходимо закупить:")
+    pprint(res, indent=1, sort_dicts=False, width=88)
+
+
+get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2)
+
+    # {
+    #     'Картофель': {'measure': 'кг', 'quantity': 2},
+    #     'Молоко': {'measure': 'мл', 'quantity': 200},
+    #     'Помидор': {'measure': 'шт', 'quantity': 4},
+    #     'Сыр гауда': {'measure': 'г', 'quantity': 200},
+    #     'Яйцо': {'measure': 'шт', 'quantity': 4},
+    #     'Чеснок': {'measure': 'зубч', 'quantity': 6}
+    # }
